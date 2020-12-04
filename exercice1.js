d3c51faa37fb9cbebc324cbd17803f5e7e467dab
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-12-04 09:38:24 +0100

    Modif

--- exercice1.js
+++ exercice1.js
@@ -1,11 +1,14 @@
-let monDiv = .createElement('div');
+let monDiv = document.createElement('div');
+let monDiv2 = document.createElement('div');
+let viewport = document.getElementById("viewport");
 
-document.getElementById('viewport').appendChild(monDiv);
+viewport.appendChild(monDiv);
+viewport.appendChild(monDiv2);
 
+monDiv.innerHTML = "Ce div a été ajouté en utilisant javascript";
 
+monDiv2.style.backgroundColor = "red";
+monDiv2.style.height = "100px";
+monDiv2.style.width = "100px";
 
-
-
-
-
-
+document.getElementById("aSupprimer").remove();
